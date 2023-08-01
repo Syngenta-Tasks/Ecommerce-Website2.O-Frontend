edf7d1dbe1760d58648dc0db54a0f059fd046b5d
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2023-08-01 22:23:04 +0530

    role based page navigation

---
+++ src/components/UserPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserPage = () => {
+  return (
+    <div  style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1  style={{ textAlign: "center",fontSize: "36px"  }}>Welcome User!</h1>
+    </div>
+  );
+};
+
+export default UserPage;
