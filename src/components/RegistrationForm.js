b83626c850c869e99faebb170ba017114068de98
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2023-08-16 21:59:09 +0530

    minor changes

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,64 +1,65 @@
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
-import styles from './RegistrationForm.module.css';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
+import styles from "./RegistrationForm.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const { Item } = Form;
 
 function RegistrationForm() {
-
   let navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirm, setConfirm] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name,email,password,confirm);
+    console.log(name, email, password, confirm);
 
     try {
-      
       const token = process.env.ACCESS_TOKEN;
-      // const response = await axios.post(`${process.env.REACT_APP_API_URL}/user/register`, {
-      const response = await axios.post(`http://localhost:3000/user/register`, {
-        name, email, password, confirm
-      },
-  
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: '*/*',
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/user/register`,
+        {
+          name,
+          email,
+          password,
+          confirm,
         },
-      }
-    );
 
-    console.log(response.data); 
-    navigate(`/welcome/${name}`);
-  } catch (error) {
-    console.error('Registration failed', error);
-  }
-};
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "*/*",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
+      console.log(response.data);
+      navigate(`/welcome/${name}`);
+    } catch (error) {
+      console.error("Registration failed", error);
+    }
+  };
 
-      
   return (
     <div className={styles.formContainer}>
-      <form name="registration" onSubmit={handleSubmit} className={styles.registrationForm}>
+      <form
+        name="registration"
+        onSubmit={handleSubmit}
+        className={styles.registrationForm}
+      >
         <h2 className={styles.heading}>Register</h2>
 
         <Item
           name="name"
           label="Name"
-          rules={[
-            { required: true, message: 'Please enter your name' },
-          ]}
+          rules={[{ required: true, message: "Please enter your name" }]}
           value={name}
-            onChange={(e) => setName(e.target.value)}
-            className={styles.input}
+          onChange={(e) => setName(e.target.value)}
+          className={styles.input}
         >
           <Input />
         </Item>
@@ -67,11 +68,12 @@ function RegistrationForm() {
           name="email"
           label="Email"
           rules={[
-            { required: true, message: 'Please enter your email' },
-            { type: 'email', message: 'Please enter a valid email' },
-          ]} value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={styles.input}
+            { required: true, message: "Please enter your email" },
+            { type: "email", message: "Please enter a valid email" },
+          ]}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={styles.input}
         >
           <Input />
         </Item>
@@ -79,11 +81,10 @@ function RegistrationForm() {
         <Item
           name="password"
           label="Password"
-          rules={[
-            { required: true, message: 'Please enter a password' },
-          ]} value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
+          rules={[{ required: true, message: "Please enter a password" }]}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className={styles.input}
         >
           <Input.Password />
         </Item>
@@ -91,15 +92,17 @@ function RegistrationForm() {
         <Item
           name="confirmPassword"
           label="Confirm Password"
-          dependencies={['password']}
+          dependencies={["password"]}
           rules={[
-            { required: true, message: 'Please confirm your password' },
+            { required: true, message: "Please confirm your password" },
             ({ getFieldValue }) => ({
               validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
+                if (!value || getFieldValue("password") === value) {
                   return Promise.resolve();
                 }
-                return Promise.reject(new Error('The two passwords do not match'));
+                return Promise.reject(
+                  new Error("The two passwords do not match")
+                );
               },
             }),
           ]}
@@ -110,7 +113,6 @@ function RegistrationForm() {
             className={styles.input}
           />
         </Item>
-
 
         <Item>
           <Button onClick={handleSubmit} type="primary" htmlType="submit">
